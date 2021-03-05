7ae26234451cf943330d407638c45d80133a04b5
sbbtt <sbbt@naver.com>
2021-03-05 10:58:52 +0900

    hide private data

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+const config = require('./config/key')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sbbt:sbbt1234@boilerplate.0p3pj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
+
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected.'))
   .catch(err=> console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세용용')
+  res.send('Hello World! HNY 2021! new changes gang')
+
+})
+
+app.post('/register', (req, res) => {
+    //회원가입 시 필요한 정보를 client 에서 db에 꽂기. router
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
